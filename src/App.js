6d6a52bf313b3d4a05a7153c4acde11ada236e1b
jradha11 <radhajayaraman11@gmail.com>
2021-11-05 00:37:14 +0530

    basic directory structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, BrowserRouter} from 'react-router-dom';
+
+/* local imports */
+import * as URLS from "./utils/AppUrls";
+import HomePage from "./containers/HomePage";
+import AboutMe from './containers/AboutMe';
+import ContactMe from './containers/ContactMe';
+import CreativeCorner from './containers/CreativeCorner';
+import Work from './containers/Work';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route 
+            exact
+            path={URLS.HOME_PAGE}
+            component = {HomePage}
+          />
+          <Route 
+            exact
+            path={URLS.ABOUT_ME}
+            component = {AboutMe}
+          />
+          <Route 
+            exact
+            path={URLS.WORK}
+            component = {Work}
+          />
+          <Route 
+            exact
+            path={URLS.CREATIVE_CORNER}
+            component = {CreativeCorner}
+          />
+          <Route 
+            exact
+            path={URLS.CONTACT_ME}
+            component = {ContactMe}
+          />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/containers/CreativeCorner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreativeCorner = () => {
+    return(
+        <div>
+            <h1>CreativeCorner</h1>
+        </div>
+    )
+}
+
+export default CreativeCorner;
