a595d8b554d676650ea7c24cc0330fe6295af01d
jradha11 <radhajayaraman11@gmail.com>
2021-11-05 00:37:25 +0530

    add app urls

---
+++ src/utils/AppUrls.js
@@ -0,0 +1,6 @@
+export const HOME_PAGE = "/";
+export const ABOUT_ME = "/about_me";
+export const WORK = "/work";
+export const CREATIVE_CORNER = "/creative_corner"
+export const RESUME = "resume";
+export const CONTACT_ME = "/contact_me"
