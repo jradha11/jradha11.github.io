76b0cd49b758a713e2f0d0812d57d3d2a87e0825
jradha11 <radhajayaraman11@gmail.com>
2021-11-06 12:40:12 +0530

    app constants

---
+++ src/utils/AppConstants.js
@@ -0,0 +1,4 @@
+export const FACEBOOK_URL = "https://www.facebook.com/radha.jayaraman.11/";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/radha-jayaraman-9b14a81a0/";
+export const GITHUB_URL = "https://github.com/jradha11";
+export const INSTAGRAM_URL = "https://www.instagram.com/_.radhajayaraman/";
