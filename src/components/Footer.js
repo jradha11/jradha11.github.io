0b6b4f7617f801da6e3043b244717b45924327d3
jradha11 <radhajayaraman11@gmail.com>
2021-11-17 14:17:12 +0530

    add footer behance link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,12 +2,14 @@ import react from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
-import "../static/styles/components/Footer.css"
+/* local imports*/
+import "../static/styles/components/Footer.css";
+import { BEHANCE_URL } from "../utils/AppConstants";
 
 const Footer = () => {
     return(
         <div className='footer'>
-            designed and developed with <FontAwesomeIcon icon={faHeart} size='xs'/> by <a href="behance link" className='footer-link'>radha</a>
+            designed and developed with <FontAwesomeIcon icon={faHeart} size='xs'/> by <a href={BEHANCE_URL} className='footer-link' target='__blank'>radha</a>
         </div>
     )
 }
--- src/utils/AppConstants.js
+++ src/utils/AppConstants.js
@@ -2,6 +2,7 @@ export const FACEBOOK_URL = "https://www.facebook.com/radha.jayaraman.11/";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/radha-jayaraman-9b14a81a0/";
 export const GITHUB_URL = "https://github.com/jradha11";
 export const INSTAGRAM_URL = "https://www.instagram.com/_.radhajayaraman/";
+export const BEHANCE_URL = "https://www.behance.net/radhajayaraman"
 
 export const PAGE_HEADING = {
     "ABOUT_ME": {
