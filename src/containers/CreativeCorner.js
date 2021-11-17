dc0b62ee0e207dbd29e8b584892e620d5b5e0f44
jradha11 <radhajayaraman11@gmail.com>
2021-11-17 15:01:23 +0530

    add creativeCorner container

--- src/containers/CreativeCorner.js
+++ src/containers/CreativeCorner.js
@@ -7,7 +7,11 @@ import "../static/styles/CommonCss.css"
 /* local imports */
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
-import { PAGE_HEADING } from "../utils/AppConstants";
+import { INSTAGRAM_URL, PAGE_HEADING } from "../utils/AppConstants";
+
+/* img import */
+import p1 from "../static/images/postcards/postcard1.jpg"
+import Footer from "../components/Footer";
 
 const CreativeCorner = () => {
     const header_data = PAGE_HEADING['CREATIVE_CORNER']
@@ -15,6 +19,21 @@ const CreativeCorner = () => {
         <div>
             <Navbar/>
             <Header header_data={header_data}/>
+            <div className='page-content-container'>
+                <div className='creative-postcards-container'>
+                    <img src={p1} alt='postcard1' className='creative-postcard'/>
+                    <img src={p1} alt='postcard1' className='creative-postcard'/>
+                    <img src={p1} alt='postcard1' className='creative-postcard'/>
+                    <img src={p1} alt='postcard1' className='creative-postcard'/>
+                    <img src={p1} alt='postcard1' className='creative-postcard'/>
+                    <img src={p1} alt='postcard1' className='creative-postcard'/>
+                </div>
+            </div>
+            <div className='view-more-container'>
+                <p className='bold-blue-text'>Liked what you saw? Checkout my insta profile to find some more!</p>
+                <a className='view-more' href={INSTAGRAM_URL}>view more</a>
+            </div>
+            <Footer/>
         </div>
     )
 }
