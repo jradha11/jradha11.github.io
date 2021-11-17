56b61f128209f14ae2b9bdaed41e533b7a7ec547
jradha11 <radhajayaraman11@gmail.com>
2021-11-17 12:21:04 +0530

    add page heading dictionary

--- src/utils/AppConstants.js
+++ src/utils/AppConstants.js
@@ -2,3 +2,22 @@ export const FACEBOOK_URL = "https://www.facebook.com/radha.jayaraman.11/";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/radha-jayaraman-9b14a81a0/";
 export const GITHUB_URL = "https://github.com/jradha11";
 export const INSTAGRAM_URL = "https://www.instagram.com/_.radhajayaraman/";
+
+export const PAGE_HEADING = {
+    "ABOUT_ME": {
+        "heading": "about me",
+        "subheading": "At some point, we all have to choose, between what the world wants you to be, and who you are."
+    },
+    "WORK": {
+        "heading": "work",
+        "subheading": "I guess I’ve been working so hard, I forgot what it’s like to be hardly working."
+    },
+    "CREATIVE_CORNER": {
+        "heading": "creative corner",
+        "subheading": "There’s a lot of beauty in ordinary things. Isn’t that kind of the point?",
+    },
+    "CONTACT_ME": {
+        "heading": "contact me",
+        "subheading": "People will never be replaced by machines. In the end, life and business are about human connections."
+    }
+}
