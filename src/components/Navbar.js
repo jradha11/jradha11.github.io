156596e432c4c5bff1c6c2dad875ce8a08828219
jradha11 <radhajayaraman11@gmail.com>
2021-11-25 19:45:55 +0530

    make navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,53 +2,89 @@ import react from "react";
 import { Link } from "react-router-dom";
 
 
-/* local imports */
+/* css imports */
 import "../static/styles/components/Navbar.css";
 import "../static/styles/CommonCss.css";
+import "../static/styles/MediaQueries.css";
+
+/* local imports */
 import logo from "../static/images/mylogo-light.svg"
 import * as URLS from "../utils/AppUrls"
+import $ from 'jquery';
 
 const Navbar = () => {
+    $(window).scroll(function() {
+            if ($(document).scrollTop() > 50) {
+                $('.nav').addClass('affix');
+            } else {
+                $('.nav').removeClass('affix');
+            }
+    });
+
+    const navTriggerOnClick = () => {
+        $('.navTrigger').toggleClass('active');
+        $("#mainListDiv").toggleClass("show_list");
+        $("#mainListDiv").fadeIn();
+
+        /* need to add fadeout */
+    }
     return(
-        <div className='navbar-web'>
-            <div className='navbar-wrapper'>
-                <Link to={URLS.HOME_PAGE} className='remove-underline'>
-                    <div className='navbar-name-container'>
-                        <img src={logo} alt="my-logo"/>
-                        <p className='navbar-name'> RADHA JAYARAMAN </p>
+            <nav className="nav">
+                <div className="nav-container">
+                    <div className="logo">
+                        <Link to={URLS.HOME_PAGE} className='remove-underline'>
+                            <div style={{display: 'flex', alignItems: 'center'}}>
+                                <img src={logo} alt="my-logo" className='my-logo'/>
+                                <p className='navbar-name'> RADHA JAYARAMAN </p>
+                            </div>
+                        </Link>
                     </div>
-                </Link>
-                <div className='navbar-content'>
-                    <Link to={URLS.ABOUT_ME} className='remove-underline'>
-                        <div className="navbar-items">
-                            about me
-                        </div>
-                    </Link>
-                    <Link to={URLS.WORK} className='remove-underline'>
-                        <div className="navbar-items">
-                            work
-                        </div>
-                    </Link>
-                    <Link to={URLS.CREATIVE_CORNER} className='remove-underline'>
-                        <div className="navbar-items">
-                            creative corner
-                        </div>
-                    </Link>
-                    <Link to={URLS.RESUME} className='remove-underline'>
-                        <div className="navbar-items">
-                            resume
-                        </div>
-                    </Link>
-                    <Link to={URLS.CONTACT_ME} className='remove-underline'>
-                        <div className="navbar-items">
-                            contact me
-                        </div>
-                    </Link>
-
+                    <div id="mainListDiv" className="main_list">
+                        <ul className="navlinks">
+                            <Link to={URLS.ABOUT_ME} className='remove-underline'>
+                                <li>
+                                    <div className="navbar-items">
+                                        about me
+                                    </div>
+                                </li>
+                            </Link>
+                            <Link to={URLS.WORK} className='remove-underline'>
+                                <li>
+                                    <div className="navbar-items">
+                                        work
+                                    </div>
+                                </li>
+                            </Link>
+                            <Link to={URLS.CREATIVE_CORNER} className='remove-underline'>
+                                <li>
+                                    <div className="navbar-items">
+                                        creative corner
+                                    </div>
+                                </li>
+                            </Link>
+                            <Link to={URLS.RESUME} className='remove-underline'>
+                                <li>
+                                    <div className="navbar-items">
+                                        resume
+                                    </div>
+                                </li>
+                            </Link>
+                            <Link to={URLS.CONTACT_ME} className='remove-underline'>
+                                <li>
+                                    <div className="navbar-items">
+                                        contact me
+                                    </div>
+                                </li>
+                            </Link>
+                        </ul>
+                    </div>
+                    <span className="navTrigger" onClick={navTriggerOnClick}>
+                        <i></i>
+                        <i></i>
+                        <i></i>
+                    </span>
                 </div>
-            </div>
-
-        </div>
+            </nav>
     )
 }
 
